5fb37a4119661c2df37c0b4f043a7986e19828ea
Bryce Boe <bbzbryce@gmail.com>
2014-01-28 12:55:58 -0800

    Add sitemap generation.

--- pelicanconf.py
+++ pelicanconf.py
@@ -13,25 +13,36 @@ TIMEZONE = 'America/Los_Angeles'
 
 DEFAULT_LANG = 'en'
 
+DEFAULT_PAGINATION = 10
+
+PLUGINS = ['plugins.sitemap']
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
 
 # Social widget
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
+# Robots.txt
 STATIC_PATHS = ['extra/robots.txt', 'images']
 EXTRA_PATH_METADATA = {'extra/robots.txt': {'path': 'robots.txt'}}
 
-DEFAULT_PAGINATION = 10
+# Sitemap
+SITEMAP = {'format': 'xml',
+           'priorities': {'articles': 1, 'indexes': 0.25, 'pages': 0.5},
+           'changefreqs': {'articles': 'monthly', 'indexes': 'daily',
+                           'pages': 'monthly'}}
+
+
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
---
+++ plugins/README.md
@@ -0,0 +1,7 @@
+# Sources
+
+I want to keep the plugins in version control within my project as it makes it
+simpler to configure the plugins. Below is a list of sources where the plugins
+can be found:
+
+* sitemap.py: https://github.com/getpelican/pelican-plugins/tree/master/sitemap
