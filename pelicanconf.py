15a1a4a483f68fc26a2fa62f8e2d15bafa0a54f6
Bryce Boe <bbzbryce@gmail.com>
2014-01-28 17:23:42 -0800

    Add links and fine-tune some settings.

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,20 +2,31 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+# Site info
 AUTHOR = 'Bryce Boe'
 SITENAME = 'Bryce Boe'
 SITEURL = ''
+SITESUBTITLE = 'The Adventures of a UCSB Computer Science Ph.D. Student'
+TWITTER_USERNAME = 'bboe'
 
-ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+# Site paths / urls
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}/'
 
-TIMEZONE = 'America/Los_Angeles'
-
+# General Settings
 DEFAULT_LANG = 'en'
-
 DEFAULT_PAGINATION = 10
-
+DISPLAY_CATEGORIES_ON_MENU = False
 PLUGINS = ['plugins.sitemap', 'pelican_gist']
+TIMEZONE = 'America/Los_Angeles'
+TYPOGRIFY = True
+
+# Disable pages
+ARCHIVES_SAVE_AS = False
+AUTHOR_SAVE_AS = False
+AUTHORS_SAVE_AS = False
+CATEGORIES_SAVE_AS = False
+TAGS_SAVE_AS = False
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -23,16 +34,17 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Adam Doupé', 'http://adamdoupe.com/'),
+         ('Julie Bifano', 'http://juliebifano.com/'))
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('+BryceBoe', 'https://plus.google.com/+BryceBoe'),
+          ('@bboe', 'https://twitter.com/bboe'),
+          ('bboe', 'https://github.com/bboe'),
+          ('bbzbryce', 'http://www.last.fm/user/bbzbryce'),
+          ('/u/bboe', 'http://www.reddit.com/user/bboe'))
 
-# Robots.txt
+# Static files (.htaccess, robots.txt)
 STATIC_PATHS = ['extra/robots.txt', 'images']
 EXTRA_PATH_METADATA = {'extra/robots.txt': {'path': 'robots.txt'}}
 
